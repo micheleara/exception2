37d02892144aca22babc45ae5773b95f77d3412a
Michele Oliveira <shellyguns@gmail.com>
2024-06-14 10:06:45 -0300

    very bad solution

---
+++ src/application/Program.java
@@ -0,0 +1,46 @@
+ 
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informe os dados da conta");
+		System.out.print("Numero: ");
+		int number = sc.nextInt();
+		System.out.print("Titular: ");
+		sc.nextLine(); //consumindo a quebra de linha do nextInt
+		String holder = sc.nextLine();
+		System.out.print("Saldo Inicial: ");
+		double balance = sc.nextDouble();
+		System.out.print("Limite de saque: ");
+		double withdrawLimit = sc.nextDouble();
+		
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.println();
+		System.out.print("Informe uma quantia para sacar: ");
+		double amount = sc.nextDouble();
+		
+		if (amount > acc.getWithdrawLimit()) {
+			System.out.println("Erro de saque: A quantia excede o limite de saque");
+		}else if (amount > acc.getBalance()) {
+			System.out.println("Erro de saque: Saldo insuficiente");
+		}else {
+			acc.withdraw(amount);
+			System.out.printf("Novo saldo: %.2f%n " ,acc.getBalance());
+		}
+		
+		
+		sc.close();
+	}
+
+}
